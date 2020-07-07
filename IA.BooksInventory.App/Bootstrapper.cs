6b4b847e99c2adf64ca411acfc92cd72a4429012
Partho P. Das <parthopdas@live.com>
2020-07-07 17:30:57 +1000

    Bringing in Caliburn.Micro app structure

---
+++ IA.BooksInventory.App/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro.Autofac;
+using IA.BooksInventory.App.ViewModels;
+
+namespace IA.BooksInventory.App
+{
+    public sealed class Bootstrapper : AutofacBootstrapper<ShellViewModel>
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ IA.BooksInventory.App/ViewModels/ShellViewModel.cs
@@ -0,0 +1,8 @@
+using Caliburn.Micro;
+
+namespace IA.BooksInventory.App.ViewModels
+{
+    public sealed class ShellViewModel : Screen
+    {
+    }
+}
